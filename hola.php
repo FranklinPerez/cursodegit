1f4b794cabe262aef60492833930e31916ddba06
Franklin Perez <franklin9perez952012@gmail.com>
2019-02-14 11:40:13 -0600

    hola.php

--- hola.php
+++ hola.php
@@ -1,3 +1,6 @@
 <?php
+// Este comentario está mal y hay que borrarlo
+$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+@print "Hola, {$nombre}\n";<?php
 echo "Hola Mundo XD\n";
 @print "Hola, {$nombre}\n";
