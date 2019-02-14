95c0f30e8f73fc5bcd78d960e6d30e9e5f7f589c
Franklin Perez <franklin9perez952012@gmail.com>
2019-02-14 12:23:22 -0600

    le puse mas lineas

--- hola.php
+++ hola.php
@@ -4,3 +4,7 @@ $nombre = isset($argv[1]) ? $argv[1] : "Mundo";
 @print "Hola, {$nombre}\n";<?php
 echo "Hola Mundo XD\n";
 @print "Hola, {$nombre}\n";
+@print "Hola, {$nombre}\n";
+@print "Hola, {$nombre}\n";
+@print "Hola, {$nombre}\n";
+
